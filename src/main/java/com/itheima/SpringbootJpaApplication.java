a0ccda0124585141d1b4630873e23994e6c259da
pyl <12866@qq.com>
2019-03-08 20:13:25 +0800

    祝福了小仙女节日快乐~

--- src/main/java/com/itheima/SpringbootJpaApplication.java
+++ src/main/java/com/itheima/SpringbootJpaApplication.java
@@ -4,15 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//小仙女是世界上最美丽的女神
-/*
-祝 小仙女
-	女神节
-	女生节
-	娃娃节
-	仙女节
-	魔镜魔镜世界上最美丽节日快乐；
- */
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
