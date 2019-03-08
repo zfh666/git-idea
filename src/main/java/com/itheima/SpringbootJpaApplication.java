fec9eaa112898dadd150d273249df97e5e069507
pyl <12866@qq.com>
2019-03-08 20:15:31 +0800

    dev代码编写

--- src/main/java/com/itheima/SpringbootJpaApplication.java
+++ src/main/java/com/itheima/SpringbootJpaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("dev代码");
 		SpringApplication.run(SpringbootJpaApplication.class, args);
 	}
 
